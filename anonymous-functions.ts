b45e5ae6054ae76f915075b4fb5dddc59c1056dc
Irene <irene.tomaini@docplanner.com>
2021-04-30 13:36:59 +0200

    update anonymous-functions

--- anonymous-functions.ts
+++ anonymous-functions.ts
@@ -15,8 +15,7 @@ printFunction(() => {
 // those functions are important because JS and Typescript use them a lot
 // es map that is an high order function
 // es filter
-// es find 
-
+// es find - find returns a value and not an array. It returns the first value that respects the rule
 
 const numbers: number[] = [1,2,3,4,5];
 // const squaredNumbers: number[] = [];
@@ -27,7 +26,6 @@ const numbers: number[] = [1,2,3,4,5];
 const squaredNumbers: number[] = numbers.map(num => num * num);
 const oddNumbers = numbers.filter(num => num % 2 !== 0);
 
-const searchNumber = 
 console.log(numbers);
 console.log(squaredNumbers);
 console.log(oddNumbers);
